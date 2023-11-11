d3e7ffe72e79159967dd008f3efc541f3ffde670
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2023-11-11 16:35:10 -0400

    add styles, add code to popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,26 @@
-console.log('Hello, this is my Chrome extension!');
+document.getElementById('submitBtn').addEventListener('click', function () {
+  const url = document.getElementById('urlInput').value;
+  if (url) {
+    // Call the API and handle the response
+    callAPI(url);
+  } else {
+    displayError('Please enter a URL.');
+  }
+});
+
+function callAPI(url) {
+  // Implement the API calling logic here
+  // Use fetch API or XMLHttpRequest to call the IFTTT Webhook
+}
+
+function displayResponse(response) {
+  const displayArea = document.getElementById('responseDisplay');
+  displayArea.innerText = response;
+  displayArea.style.display = 'block';
+}
+
+function displayError(error) {
+  const errorArea = document.getElementById('errorDisplay');
+  errorArea.innerText = error;
+  errorArea.style.display = 'block';
+}
